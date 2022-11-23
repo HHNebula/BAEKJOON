b7cd90968626d420cd5c7b1cc68b1134bc7473ac
SOO <111834203+HHNebula@users.noreply.github.com>
2022-11-23 23:55:27 +0900

    Create 2742.java

---
+++ etc/class1/2742.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+//		문제
+//		자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+//		입력
+//		첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+
+//		출력
+//		첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+
+		for (int i = num; i > 0; i--) {
+			System.out.println(i);
+		}
+
+		scanner.close();
+
+	}
+
+}
