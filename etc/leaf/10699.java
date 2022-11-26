212a9db254352cc1d485fca9d28d948b9e1a047c
SOO <111834203+HHNebula@users.noreply.github.com>
2022-11-26 13:11:21 +0900

    Create 10699.java

---
+++ etc/leaf/10699.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+//		문제
+//		서울의 오늘 날짜를 출력하는 프로그램을 작성하시오.
+
+//		입력
+//		입력은 없다.
+
+//		출력
+//		서울의 오늘 날짜를 "YYYY-MM-DD" 형식으로 출력한다.
+		
+		LocalDate date = LocalDate.now();
+		
+		System.out.println(date);
+		
+	}
+	
+}
